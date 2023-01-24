af66236b3da88aaaa837bd15b361c453ba69a93d
GabrielHJLC <gabrielhenriquejlc@gmail.com>
2023-01-23 22:08:34 -0300

    project

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const $html = document.querySelector('html')
-const $switch = document.querySelector('#switch')
+const $switch = document.querySelector('.darkswitch')
 modeText = document.querySelector('.modetext')
 
 $switch.addEventListener('change',function() {
     $html.classList.toggle('dark-mode')
+    $switch.classList.toggle('active')
 
     if($html.classList.contains("dark-mode")){
         modeText.innerText = "Dark Mode";
     }else{
         modeText.innerText = "Light mode";
+        $switch.classList.toggle('not-active')
     }
 
     var theme;
@@ -27,6 +29,8 @@ let GetTheme = JSON.parse(localStorage.getItem('PageTheme'));
 if(GetTheme === 'Dark'){
     $html.classList.toggle('dark-mode')
     modeText.innerText = "Dark Mode";
+    $switch.classList.toggle('active')
 }else{
     modeText.innerText = "Light mode";
+    $switch.classList.toggle('not-active')
 }
